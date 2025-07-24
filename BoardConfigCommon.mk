ff5c04def0d509e089c958e20301b0e5d35ad9eb
uwu-gl <moegluwu0@gmail.com>
2025-07-25 07:47:26 +0800

    Revert "sm8550-common: Drop OPLUS SEPolicy for now"
    This reverts commit f8428fbb1167d477f24de09a477eae48765b1f22.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,7 +145,7 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-#include hardware/oplus/sepolicy/qti/SEPolicy.mk
+include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
