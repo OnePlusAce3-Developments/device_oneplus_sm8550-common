987c5398009e172bbcc891f01557a48cce3137a9
SakuraKyuo <sakurakyuo@protonmail.com>
2025-07-27 17:22:22 +0800

    sm8550-common: Drop livedisplay for now
    Change-Id: Ief5f8e531023e860dc9da8b1653f16d6b32d9bdd
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- common.mk
+++ common.mk
@@ -211,8 +211,8 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.oplus
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.livedisplay@2.1-service.oplus
 
 # Media
 PRODUCT_COPY_FILES += \
