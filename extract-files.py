6dc52f2ed3781cf6039c4c5396ccb271f1758daf
chandu078 <chandudyavanapelli03@gmail.com>
2025-08-21 08:43:15 +0000

    sm8550-common: Add oplus charger and horae service
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -42,6 +42,7 @@ lib_fixups: lib_fixups_user_type = {
     (
         'libpwirisfeature',
         'libpwirishalwrapper',
+        'vendor.oplus.hardware.performance-V1-ndk',
     ): lib_fixup_odm_suffix,
     (
         'com.qualcomm.qti.dpm.api@1.0',
